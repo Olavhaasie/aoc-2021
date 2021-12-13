a811ae064cbcc81d0227cb75c87bd033db7911f7
Olav de Haas <23523462+Olavhaasie@users.noreply.github.com>
2021-12-13 17:33:44 +0100

    Remove downloading of input

--- newday.sh
+++ newday.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir -p "$1"
-wget -O "$1/input.txt" "https://adventofcode.com/2021/day/$1/input"
+touch "$1/input.txt"
 cp template.idr "$1/Main.idr"
 cp template.ipkg "$1/solution.ipkg"
 
